928699edd353bf8d7a387bca25cd986707858830
Eri <103050349+Eriarer@users.noreply.github.com>
2023-11-01 19:42:31 -0600

    Add dynamic favicon based on user's color scheme preference

--- include/session.php
+++ include/session.php
@@ -3,3 +3,27 @@ session_start();
 
 // definir el path del server
 $_SERVER['ROOT'] = "http://" . $_SERVER['SERVER_NAME'] . "/Etica/";
+?>
+
+<head>
+  <!-- favIcon -->
+  <link rel="shortcut icon" type="image/png" href="#" />
+</head>
+
+<script>
+  window.matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', ({
+      matches
+    }) => {
+      if (matches) {
+        // asignar favIcon sin utilizar el ID
+        document.querySelector("link[rel*='icon']").href = "<?php echo $_SERVER['ROOT'] . 'media/images/favIconLigth.png' ?>";
+      } else {
+        document.querySelector("link[rel*='icon']").href = "<?php echo $_SERVER['ROOT'] . 'media/images/favIconDark.png' ?>";
+      }
+    });
+
+  // asignar favIcon utilizando obteniendo el color del tema
+  const theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'Ligth' : 'Dark';
+  document.querySelector("link[rel*='icon']").href = "<?php echo $_SERVER['ROOT'] . 'media/images/favIcon' ?>" + theme + ".png";
+</script>
