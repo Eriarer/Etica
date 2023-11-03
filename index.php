f4398cfab9a9360cdf34d11f8baa63033f884a83
mariana-avila-rivera <marianaavilarivera@gmail.com>
2023-11-02 18:03:08 -0600

    Co-authored: Gerardo. Distribucion + Img index

--- index.php
+++ index.php
@@ -24,28 +24,31 @@ include_once 'include/session.php';
   <div class="contenedor">
     <h1 class="titulo">El Síndrome del Impostor</h1>
     <h3>¿Qué es el síndrome del impostor? </h3>
+    <div class="image"><img src="media/images/index/imposter-syndrome.jpg" alt=""></div>
     <p>El síndrome del impostor es una sensación de inseguridad y autodesvalorización relacionada con los logros laborales, que lleva a quienes lo experimentan a creer que no merecen sus éxitos y que son un fraude. Una persona con síndrome del impostor cree que sus logros o triunfos son producto de “un golpe de suerte” o de la ayuda de los demás, pero no de su esfuerzo, capacidad, talento o creatividad.</p>
     <p>El síndrome del impostor tiene un origen multifactorial que abarca factores biológicos, psicológicos y sociales. A menudo se relaciona con problemas afectivos como la depresión, trastornos de ansiedad generalizada y trastorno por déficit de atención e hiperactividad. </p>
     <p>Cualquier persona, independientemente de su género, edad o profesión, puede experimentar este síndrome, que no está clasificado como un trastorno psicológico clínico, pero puede tener un impacto significativo en la autoestima y el bienestar emocional de quienes lo padecen.</p>
 
     <h3>Síntomas comunes</h3>
     <p>Los síntomas principales asociados al síndrome del impostor pueden variar de una persona a otra. Aunque es importante señalar que no todas las personas experimentan todos los síntomas, algunos de los signos comunes incluyen:</p>
-    <ul>
-      <li>Creencia de no merecer los logros obtenidos. Piensan que no son justos o que se han obtenido gracias a un golpe de suerte.</li>
-      <li>Insatisfacción permanente.</li>
-      <li>Basar tu autoestima en la forma en que percibes tus propias habilidades.</li>
-      <li>Desconfianza hacia sus propias capacidades.</li>
-      <li>Temor constante a ser percibido como un fraude por sus logros.</li>
-      <li>Sentirte solo, o la necesidad de aislarte, para que nadie descubra tu “secreto”. </li>
-      <li>Desmotivación por falta de confianza en sí mismo.</li>
-      <li>Expectativas de fracaso ante cualquier situación.</li>
-      <li>Sentimientos de tristeza y desesperanza.</li>
-      <li>Ansiedad y depresión.</li>
-      <li>Sacrifica tu propio bienestar para realizar más trabajo.</li>
-      <li>Sentir que necesitas ser un perfeccionista para producir un trabajo satisfactorio. </li>
-      <li>Deterioro de tu salud mental como resultado del exceso de trabajo y el agotamiento.</li>
-    </ul>
-
+    <div class="dividedList">
+      <img src="media/images/index/sintomas-SI-2.png" alt="">
+      <ul>
+        <li>Creencia de no merecer los logros obtenidos. Piensan que no son justos o que se han obtenido gracias a un golpe de suerte.</li>
+        <li>Insatisfacción permanente.</li>
+        <li>Basar tu autoestima en la forma en que percibes tus propias habilidades.</li>
+        <li>Desconfianza hacia sus propias capacidades.</li>
+        <li>Temor constante a ser percibido como un fraude por sus logros.</li>
+        <li>Sentirte solo, o la necesidad de aislarte, para que nadie descubra tu “secreto”. </li>
+        <li>Desmotivación por falta de confianza en sí mismo.</li>
+        <li>Expectativas de fracaso ante cualquier situación.</li>
+        <li>Sentimientos de tristeza y desesperanza.</li>
+        <li>Ansiedad y depresión.</li>
+        <li>Sacrifica tu propio bienestar para realizar más trabajo.</li>
+        <li>Sentir que necesitas ser un perfeccionista para producir un trabajo satisfactorio. </li>
+        <li>Deterioro de tu salud mental como resultado del exceso de trabajo y el agotamiento.</li>
+      </ul>
+    </div>
     <h3>Causas y factores desencadenantes</h3>
     <p> Las posibles causas que se relacionan con el síndrome del impostor son:</p>
     <ul>
@@ -64,20 +67,33 @@ include_once 'include/session.php';
     <p>El síndrome del impostor es más común en profesiones que requieren una alta competencia, como la ciencia, la tecnología, la ingeniería y las matemáticas (STEM), así como en profesiones que requieren una alta exposición pública, como la actuación, la música y el deporte.</p>
 
     <h3>Impacto en la vida personal y profesional</h3>
-    <p>El síndrome del impostor puede tener un impacto significativo en la vida personal y profesional de las personas. A nivel personal, el síndrome del impostor puede provocar ansiedad, depresión, aislamiento y baja autoestima. A nivel profesional, el síndrome del impostor puede dificultar la toma de riesgos, la búsqueda de oportunidades y el desarrollo de la carrera.</p>
-    <p>El síndrome del impostor puede afectar las relaciones interpersonales de las personas. Las personas que sufren de este síndrome pueden ser reticentes a compartir sus logros con los demás, por miedo a ser juzgadas. Esto puede conducir a la soledad y al aislamiento.</p>
-    <p>El síndrome del impostor puede afectar la carrera de las personas. Las personas que sufren de este síndrome pueden ser reacias a pedir ascensos o nuevas responsabilidades, por miedo a no estar a la altura. Esto puede limitar su desarrollo profesional.</p>
+    <div class="dividedList">
+      <div class="dividedText">
+        <p>El síndrome del impostor puede tener un impacto significativo en la vida personal y profesional de las personas. A nivel personal, el síndrome del impostor puede provocar ansiedad, depresión, aislamiento y baja autoestima. A nivel profesional, el síndrome del impostor puede dificultar la toma de riesgos, la búsqueda de oportunidades y el desarrollo de la carrera.</p>
+        <p>El síndrome del impostor puede afectar las relaciones interpersonales de las personas. Las personas que sufren de este síndrome pueden ser reticentes a compartir sus logros con los demás, por miedo a ser juzgadas. Esto puede conducir a la soledad y al aislamiento.</p>
+        <p>El síndrome del impostor puede afectar la carrera de las personas. Las personas que sufren de este síndrome pueden ser reacias a pedir ascensos o nuevas responsabilidades, por miedo a no estar a la altura. Esto puede limitar su desarrollo profesional.</p>
+      </div>
+      <img src="media/images/index/tormento-fantasmas-V.jpg" alt="">
+    </div>
 
     <h3>Estrategias de afrontamiento</h3>
-    <p>Es importante señalar que el síndrome del impostor no es un trastorno psicológico, sino un fenómeno relativamente común.</p>
-    <p>Hay una serie de estrategias de afrontamiento que pueden ayudar a las personas a superar el síndrome del impostor. Estas estrategias incluyen:</p>
-    <ul>
-      <li>Reconocer los pensamientos y sentimientos negativos. El primer paso para superar el síndrome del impostor es ser consciente de los pensamientos y sentimientos negativos que lo acompañan. Una vez que se identifican estos pensamientos, se pueden empezar a desafiar y sustituir por otros más positivos.</li>
-      <li>Practicar la autoafirmación. Es importante recordar a uno mismo las propias fortalezas y logros. Esto se puede hacer escribiendo una lista de las propias cualidades y éxitos, o manteniendo un diario de agradecimiento.</li>
-      <li>Buscar apoyo. Hablar con un amigo, familiar o terapeuta puede ayudar a las personas a sentirse menos solas y a desarrollar estrategias para afrontar el síndrome del impostor.</li>
-      <li>Aceptar la imperfección. Es importante recordar que nadie es perfecto. Todos cometemos errores y todos tenemos áreas en las que podemos mejorar.</li>
-    </ul>
-
+    <div class="divided">
+      <div class="dividedText">
+        <p>Es importante señalar que el síndrome del impostor no es un trastorno psicológico, sino un fenómeno relativamente común.</p>
+      </div>
+      <div class="dividedList">
+        <img src="media/images/index/ilustracion-alta-autoestima.jpg" alt="">
+        <div>
+          <p>Hay una serie de estrategias de afrontamiento que pueden ayudar a las personas a superar el síndrome del impostor. Estas estrategias incluyen:</p>
+          <ul>
+            <li>Reconocer los pensamientos y sentimientos negativos. El primer paso para superar el síndrome del impostor es ser consciente de los pensamientos y sentimientos negativos que lo acompañan. Una vez que se identifican estos pensamientos, se pueden empezar a desafiar y sustituir por otros más positivos.</li>
+            <li>Practicar la autoafirmación. Es importante recordar a uno mismo las propias fortalezas y logros. Esto se puede hacer escribiendo una lista de las propias cualidades y éxitos, o manteniendo un diario de agradecimiento.</li>
+            <li>Buscar apoyo. Hablar con un amigo, familiar o terapeuta puede ayudar a las personas a sentirse menos solas y a desarrollar estrategias para afrontar el síndrome del impostor.</li>
+            <li>Aceptar la imperfección. Es importante recordar que nadie es perfecto. Todos cometemos errores y todos tenemos áreas en las que podemos mejorar.</li>
+          </ul>
+        </div>
+      </div>
+    </div>
     <h4>Bibliografía</h4>
     <ul>
       <li><a href="https://asana.com/es/resources/impostor-syndrome">https://asana.com/es/resources/impostor-syndrome</a></li>
