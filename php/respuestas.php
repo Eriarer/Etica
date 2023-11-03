f25444f09b4f3ec154fb1b7d3df5d084e4ecab45
Eriarer <eriarer_melgoza+git@hotmail.com>
2023-11-02 19:29:13 -0600

    score update

--- php/respuestas.php
+++ php/respuestas.php
@@ -1,8 +1,7 @@
 <?php
 include_once '../include/session.php';
 
-if ($_SERVER['REQUEST_METHOD'] != 'POST')
-{
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     header('Location: ../index.php');
 }
 
@@ -25,23 +24,35 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST')
 <style>
     <?php
     $puntos = 0;
-    for ($i = 1; $i <= 20; $i++)
-    {
+    for ($i = 1; $i <= 20; $i++) {
         $p = $_POST["p" . $i];
         $puntos = $puntos + $p;
     }
+    $puntos = $puntos;
+    $steps = $puntos / $puntos;
+    // generar un arreglo de $puntos elementos de color rojo -> rgb(255, 59, 48) a verde -> rgb(38, 198, 62)
+    $colors = array();
+    $red = 255;
+    $green = 59;
+    $blue = 48;
+    $redStep = ($red - 38) / $puntos;
+    $greenStep = ($green - 198) / $puntos;
+    $blueStep = ($blue - 62) / $puntos;
+    for ($i = 0; $i <= $puntos; $i++) {
+        $colors[$i] = 'rgb(' . $red . ', ' . $green . ', ' . $blue . ')';
+        $red = $red - $redStep;
+        $green = $green - $greenStep;
+        $blue = $blue - $blueStep;
+    }
 
-    $decil = $puntos / 10;
+
     // 360 = 100pts
     $degree = 3.6; // aumento de grados por punto
     $angle = $puntos * $degree;
-    if ($angle > 180)
-    {
+    if ($angle > 180) {
         $animationRight = 'rotate(180deg)';
         $animationLeft = 'rotate(' . ($angle - 180) . 'deg)';
-    }
-    else
-    {
+    } else {
         $animationRight = 'rotate(' . $angle . 'deg)';
         $animationLeft = 'rotate(0deg)';
     }
@@ -57,59 +68,45 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST')
         font-weight: 600;
         color: #080;
         content: '<?php echo $puntos; ?>';
-        animation: score-counter 1.2s linear forwards;
+        <?php
+        if ($animationLeft == 'rotate(0deg)') {
+            echo 'animation: score-counter 0.6s linear forwards;';
+        } else {
+            echo 'animation: score-counter 1.2s linear forwards;';
+        }
+        ?>
     }
 
     @keyframes score-counter {
-        0% {
-            content: '<?php echo $decil; ?>';
-            color: #ff3b30;
+        <?php
+        for ($i = 0; $i <= $puntos; $i++) {
+            echo ($i * (100 / $puntos)) . "% {
+                color: " . $colors[$i] . ";
+                content: '" . number_format($i * $steps, 0, '', '') . "';
+            }";
         }
+        ?>
+    }
 
-        20% {
-            content: '<?php echo $decil * 2; ?>';
-            color: #ff3b30;
+    .circle-text-color-anim {
+        background: none;
+        <?php
+        if ($animationLeft == 'rotate(0deg)') {
+            echo 'animation: circle-text-color-anim 0.6s linear forwards;';
+        } else {
+            echo 'animation: circle-text-color-anim 1.2s linear forwards;';
         }
+        ?>
+    }
 
-        30% {
-            content: '<?php echo $decil * 3; ?>';
-            color: #ff3b30;
+    @keyframes circle-text-color-anim {
+        <?php
+        for ($i = 0; $i <= $puntos; $i++) {
+            echo ($i * (100 / $puntos)) . "% {
+                color: " . $colors[$i] . ";
+            }";
         }
-
-        40% {
-            content: '<?php echo $decil * 4; ?>';
-            color: #ff3b30;
-        }
-
-        50% {
-            content: '<?php echo $decil * 5; ?>';
-            color: #ff9500;
-        }
-
-        60% {
-            content: '<?php echo $decil * 6; ?>';
-            color: #ff9500;
-        }
-
-        70% {
-            content: '<?php echo $decil * 7; ?>';
-            color: #28cd41;
-        }
-
-        80% {
-            content: '<?php echo $decil * 8; ?>';
-            color: #28cd41;
-        }
-
-        90% {
-            content: '<?php echo $decil * 9; ?>';
-            color: #28cd41;
-        }
-
-        100% {
-            content: '<?php echo $puntos; ?>';
-            color: #26C63E;
-        }
+        ?>
     }
 
     @keyframes progress-right {
@@ -129,10 +126,11 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST')
     <?php
     include_once '../include/navbar.php';
     ?>
+
     <div class="circle-bg-color-anim score-circular">
         <div class="circle-bg-color-anim score-inner"></div>
         <div class="score-number"></div>
-        <div class="circle-text-color-anim score-text">De 100</div>
+        <div class="circle-text-color-anim score-text">100</div>
         <div class="score-circle">
             <div class="circle-bg-color-anim score-bar progress-right">
                 <div class="circle-bg-color-anim score-progress"></div>
