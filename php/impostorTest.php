788c2cbfda56ae89e8e83107c7ac8ecadc6b4bd5
Eri <103050349+Eriarer@users.noreply.github.com>
2023-11-03 20:34:15 -0600

    Update cuestionario array in impostorTest.php

--- php/impostorTest.php
+++ php/impostorTest.php
@@ -2,25 +2,38 @@
 include_once '../include/session.php';
 
 $cuestionario = array(
-  'He tenido éxito en una prueba o tarea a pesar de que temía no hacerlo bien antes de emprender la tarea.',
+  //'He tenido éxito en una prueba o tarea a pesar de que temía no hacerlo bien antes de emprender la tarea.',
+  'He tenido éxito en una prueba a pesar de mi temor a no hacerlo bien al principio.',
   'Puedo dar la impresión de que soy más competente de lo que realmente soy.',
   'Evito las evaluaciones si es posible y tengo miedo de que otros me evalúen.',
-  'Cuando la gente me elogia por algo que he logrado, tengo miedo de no poder estar a la altura de sus expectativas hacia mí en el futuro.',
-  'A veces pienso que obtuve mi puesto actual o logré mi éxito actual porque tuve la suerte de estar en el lugar correcto en el momento adecuado o conocí a las personas adecuadas.',
-  'Tengo miedo de que las personas importantes para mí descubran que no soy tan capaz como ellos piensan.',
-  'Tiendo a recordar los incidentes en los que no he hecho lo mejor más que aquellas veces en las que he dado lo mejor de mí.',
+  //'Cuando la gente me elogia por algo que he logrado, tengo miedo de no poder estar a la altura de sus expectativas hacia mí en el futuro.',
+  'Me preocupa no estar a la altura de las expectativas cuando me elogian por mis logros.',
+  //'A veces pienso que obtuve mi puesto actual o logré mi éxito actual porque tuve la suerte de estar en el lugar correcto en el momento adecuado o conocí a las personas adecuadas.',
+  'A menudo pienso que mi puesto actual se debe a la suerte o a algún otro factor externo.',
+  //'Tengo miedo de que las personas importantes para mí descubran que no soy tan capaz como ellos piensan.',
+  'Tengo miedo de que las personas que me importan, descubran que no soy tan capaz como ellos piensan.',
+  //'Tiendo a recordar los incidentes en los que no he hecho lo mejor más que aquellas veces en las que he dado lo mejor de mí.',
+  'Recuerdo más las situaciones en las que no me he esforzado mucho que aquellas en las que di lo mejor de mí.',
   'Rara vez hago un proyecto o tarea tan bien como me gustaría hacerlo.',
-  'A veces siento o creo que mi éxito en mi vida o en mi trabajo ha sido el resultado de algún tipo de error.',
+  //'A veces siento o creo que mi éxito en mi vida o en mi trabajo ha sido el resultado de algún tipo de error.',
+  'Siento que el éxito en mi vida o trabajo es resultado de algún tipo de error.',
   'Me cuesta aceptar cumplidos o elogios sobre mi inteligencia o logros.',
   'A veces siento que mi éxito ha sido debido a algún tipo de suerte.',
-  'A veces estoy decepcionado con mis logros actuales y pienso que debería haber logrado mucho más.',
-  'A veces tengo miedo de que otros descubran cuánto conocimiento o habilidad me falta realmente.',
+  //'A veces estoy decepcionado con mis logros actuales y pienso que debería haber logrado mucho más.',
+  'A veces estoy decepcionado con mis logros y pienso que debería haber logrado mucho más.',
+  //'A veces tengo miedo de que otros descubran cuánto conocimiento o habilidad me falta realmente.',
+  'Suelo temer que otros descubran que no soy tan inteligente o hábil como piensan.',
   'A menudo tengo miedo de que pueda fallar en una nueva tarea o empresa aunque generalmente me va bien en lo que intento.',
-  'Cuando he tenido éxito en algo y he recibido reconocimiento por mis logros, dudo de que pueda seguir repitiendo ese éxito.',
-  'Si recibo muchos elogios y reconocimiento por algo que he logrado, tiendo a restar importancia a la importancia de lo que he hecho.',
-  'A menudo comparo mi capacidad con la de quienes me rodean y pienso que pueden ser más inteligentes que yo.',
-  'A menudo me preocupa no tener éxito en un proyecto o examen, aunque los demás a mi alrededor confíen considerablemente en que lo haré bien.',
-  'Si voy a recibir un ascenso o ganar reconocimiento de algún tipo, dudo en contárselo a los demás hasta que sea un hecho consumado.',
+  //'Cuando he tenido éxito en algo y he recibido reconocimiento por mis logros, dudo de que pueda seguir repitiendo ese éxito.',
+  'Dudo de poder repetir algún éxito cuando recibo reconocimientos por mis logros.',
+  //'Si recibo muchos elogios y reconocimiento por algo que he logrado, tiendo a restar importancia a la importancia de lo que he hecho.',
+  'Al recibir muchos elogios o reconocimientos por algún logro, tiendo a quitarle importancia al logro.',
+  //'A menudo comparo mi capacidad con la de quienes me rodean y pienso que pueden ser más inteligentes que yo.',
+  'Suelo comparar mi capacidad con la de quienes me rodean y pienso que pueden ser más inteligentes que yo.',
+  //'A menudo me preocupa no tener éxito en un proyecto o examen, aunque los demás a mi alrededor confíen considerablemente en que lo haré bien.',
+  'A menudo me preocupa no tener éxito en algo, a pesar de que los demás confíen en que lo haré bien.',
+  //'Si voy a recibir un ascenso o ganar reconocimiento de algún tipo, dudo en contárselo a los demás hasta que sea un hecho consumado.',
+  'Si voy a recibir un ascenso o ganar algún reconocimiento, dudo en contárselo a los demás hasta que haya sucedido.',
   'Me siento mal y desanimado si no soy "el mejor" o al menos "muy especial" en situaciones que involucran logros.'
 );
 
@@ -44,22 +57,23 @@ $cuestionario = array(
   <?php
   include_once '../include/navbar.php';
   ?>
-  <form class="container" action="respuestas.php" method="post" id="myForm">
+  <form class="myForm" action="respuestas.php" method="post" id="myForm">
     <div class="row row-cols-1 row-cols-sm-1 row-cols-lg-2">
       <?php
       $resuestas = array('nunca', 'raro', 'aveces', 'frecuente', 'siempre');
+      $meaning = array('Nunca', 'Rara vez', 'A veces', 'Frecuentemente', 'Siempre');
       $nfClass = array('nf-md-thumb_down', 'nf-md-thumb_down_outline', 'nf-md-thumbs_up_down_outline', 'nf-md-thumb_up_outline', 'nf-md-thumb_up');
 
       for ($i = 1, $j = 1; $i <= 20; $i++)
       {
         echo '<div class="col">';
-        echo ' <div class="card card-body  mx-1 mx-sm-1 mx-md-3 my-sm-1 my-md-2 p-0">';
-        echo '  <div class="h4 card-title" id="q' . $i . '">' . $i . '.-' . $cuestionario[$i - 1] . '</div>';
+        echo ' <div class="card card-body  mx-1 mx-sm-1 mx-md-3 mx-lg-5 my-2 my-sm-3 my-md-4 my-lg-5 p-0">';
+        echo '  <div class="h4 card-title question" id="q' . $i . '"><div class="nQuestion">' . $i . '</div>' . $cuestionario[$i - 1] . '</div>';
         echo '  <div class="radioInput text-center">';
         for ($j = 0; $j < 5; $j++)
         {
-          echo ' <input class="form-check-input position-static" type="radio" name="p' . $i . '" id="p' . $i . 'r' . $j . '" value="1">
-                <label for="p' . $i . 'r' . $j . '" class="nf ' . $nfClass[$j] . ' mx-3 mx-md-3 mx-lg-4 radio ' . $resuestas[$j] . ' "></label>';
+          echo ' <input class="form-check-input position-static" type="radio" name="p' . $i . '" id="p' . $i . 'r' . $j . '" value="' . ($j + 1) . '">';
+          echo  '<label for="p' . $i . 'r' . $j . '" class="nf ' . $nfClass[$j] . ' mx-3 mx-md-3 mx-lg-4 radio ' . $resuestas[$j] . ' "></label></label>';
         }
         echo '  </div>';
         echo ' </div>';
