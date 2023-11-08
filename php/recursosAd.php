d27dd89804ec5c71828a01dfa3af7e035d6971c0
Laxlev <leofrivgames1@gmail.com>
2023-11-07 21:08:27 -0600

    Info, prerelase

---
+++ php/recursosAd.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/tooltip.css">
+    <link rel="stylesheet" href="../css/index.css">
+    <?php
+        include_once '../include/session.php';
+        include_once '../include/navbar.php';
+    ?>
+</head>
+<body>
+    <div class="contenedor">
+    <h1 class="titulo">El sindrome del impostor</h1>
+    <h3>Recursos adicionales</h3>
+    <p>¡Bienvenido a nuestra página de recursos adicionales sobre el síndrome del impostor! Si buscas más información, consejos y perspectivas sobre este fenómeno común, estás en el lugar correcto. Aquí encontrarás una lista de enlaces a fuentes externas, libros, charlas y más, que te ayudarán a profundizar en el tema desde diversas perspectivas. Continúa tu exploración y amplía tu comprensión del síndrome del impostor consultando estas valiosas fuentes adicionales.</p>
+    <h4>Audiovisual</h4>
+    <p>Este video explora el síndrome del impostor, que se caracteriza por sentirse como un impostor en la vida. Se discuten las raíces del síndrome, patrones cognitivos y cómo afecta a las personas en su vida.</p>
+    <center>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/Z4P40hzv7OI?si=fNhwzZENgwuQHH69" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </center>
+        <div class="dividedList">
+            <p><br>En este segundo video, la persona comparte sus luchas con el síndrome del impostor y cómo ha afectado su vida. Habla de sus sentimientos de no pertenecer en varias situaciones, desde su trabajo hasta su experiencia de convertir a su perro en un perro de servicio. También destaca la importancia de encontrar comunidades en las que uno se sienta aceptado y representado.</p>
+            <center>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/g6wbCf0gNSw?si=dyjYtC7Trvq1uSh6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </center>
+        </div>
+    </div>
+</body>
+</html>
