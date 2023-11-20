a9ebc3f4344a5b7ffb153aa197f16d116b3babd7
mariana-avila-rivera <marianaavilarivera@gmail.com>
2023-11-19 23:08:39 -0600

    agregando enlace a sus estilos desde root

--- include/navbar.php
+++ include/navbar.php
@@ -1,16 +1,14 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand NerdFont" href="<?php echo $_SERVER['ROOT']  ?>"><i class="nf nf-custom-home"></i></a>
+<link rel="stylesheet" href="<?php echo $_SERVER['ROOT'] . 'css/navbar.css' ?>">
+
+<nav class="navbar navbar-expand-lg nav-body">
+  <a class="navbar-brand NerdFont" href="<?php echo $_SERVER['ROOT']  ?>"><i class="nf nf-custom-home navbarIcon"></i></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-
       <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo $_SERVER['ROOT'] . 'php/impostorTest/impostorTest.php' ?>"><i class="nf nf-md-brain"></i> Test</a>
+        <a class="nav-link" href="<?php echo $_SERVER['ROOT'] . 'php/impostorTest/impostorTest.php' ?>"><i class="nf nf-md-brain navbarIcon"></i> Test</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="<?php echo $_SERVER['ROOT'] . 'php/recursosAd.php' ?>">Recursos adicionales</a>
