e033e2780507c40075110150b0d7b26edb0b7096
mariana-avila-rivera <marianaavilarivera@gmail.com>
2023-11-19 23:04:53 -0600

    Incluyendo estilos "basics" y arch footer

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ include_once 'include/session.php';
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/tooltip.css">
   <link rel="stylesheet" href="css/index.css">
+  <link rel="stylesheet" href="css/basis.css">
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 </head>
@@ -94,19 +95,10 @@ include_once 'include/session.php';
         </div>
       </div>
     </div>
-    <h4>Bibliografía</h4>
-    <ul>
-      <li><a href="https://asana.com/es/resources/impostor-syndrome">https://asana.com/es/resources/impostor-syndrome</a></li>
-      <li><a href="https://www.gaceta.unam.mx/que-es-el-sindrome-del-impostor/">https://www.gaceta.unam.mx/que-es-el-sindrome-del-impostor/</a></li>
-      <li><a href="https://unamglobal.unam.mx/global_revista/sindrome-del-impostor-afecta-al-70-de-la-poblacion-de-que-se-trata/#:~:text=Siete%20de%20cada%2010%20personas,%2C%20capacidad%2C%20talento%20o%20creatividad.">https://unamglobal.unam.mx/global_revista/sindrome-del-impostor-afecta-al-70-de-la-poblacion-de-que-se-trata/</a></li>
-      <li><a href="https://www.unir.net/salud/revista/sindrome-del-impostor/">https://www.unir.net/salud/revista/sindrome-del-impostor/</a></li>
-      <li><a href="https://scholarworks.uvm.edu/tvc/vol34/iss1/6/">https://scholarworks.uvm.edu/tvc/vol34/iss1/6/</a></li>
-      <li><a href="https://so06.tci-thaijo.org/index.php/IJBS/article/view/521">https://so06.tci-thaijo.org/index.php/IJBS/article/view/521</a></li>
-      <li><a href="https://www.essentiallysports.com/f1-news-i-beat-myself-a-lot-over-it-mclarens-norris-opens-up-on-mental-struggles/">https://www.essentiallysports.com/f1-news-i-beat-myself-a-lot-over-it-mclarens-norris-opens-up-on-mental-struggles/</a></li>
-      <li><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7174434/">https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7174434/</a></li>
-      <li><a href="https://www.terapify.com/blog/sindrome-del-impostor-que-es-y-como-superarlo/">https://www.terapify.com/blog/sindrome-del-impostor-que-es-y-como-superarlo/</a></li>
-    </ul>
   </div>
+  <?php
+  include_once 'include/footer.php';
+  ?>
 </body>
 
 </html>
