f6470a78a5a59b410923d8b59bfab23b791dfa1e
mariana-avila-rivera <marianaavilarivera@gmail.com>
2023-11-19 23:09:19 -0600

    agregando footer y quitando el <strong> de tabla

--- php/recursosAd.php
+++ php/recursosAd.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
@@ -12,26 +13,27 @@
     <link rel="stylesheet" href="../css/tooltip.css">
     <link rel="stylesheet" href="../css/recursosAd.css">
     <?php
-        include_once '../include/session.php';
-        include_once '../include/navbar.php';
+    include_once '../include/session.php';
+    include_once '../include/navbar.php';
     ?>
 </head>
+
 <body>
     <div class="principal">
-    <h1 class="titulo">El sindrome del impostor</h1>
-    <h3>Recursos adicionales</h3>
-    <p>¡Bienvenido a nuestra página de recursos adicionales sobre el síndrome del impostor! Si buscas más información, consejos y perspectivas sobre este fenómeno común, estás en el lugar correcto. Aquí encontrarás una lista de enlaces a fuentes externas, libros, charlas y más, que te ayudarán a profundizar en el tema desde diversas perspectivas. Continúa tu exploración y amplía tu comprensión del síndrome del impostor consultando estas valiosas fuentes adicionales.</p>
-    <hr>
-    <h1>Audiovisual</h1>
-    <br>
+        <h1 class="titulo">El sindrome del impostor</h1>
+        <h3>Recursos adicionales</h3>
+        <p>¡Bienvenido a nuestra página de recursos adicionales sobre el síndrome del impostor! Si buscas más información, consejos y perspectivas sobre este fenómeno común, estás en el lugar correcto. Aquí encontrarás una lista de enlaces a fuentes externas, libros, charlas y más, que te ayudarán a profundizar en el tema desde diversas perspectivas. Continúa tu exploración y amplía tu comprensión del síndrome del impostor consultando estas valiosas fuentes adicionales.</p>
+        <hr>
+        <h1>Audiovisual</h1>
+        <br>
         <div class="dividedList">
             <center>
-            <ul>
-            <iframe width="700" height="380" src="https://www.youtube.com/embed/Z4P40hzv7OI?si=fNhwzZENgwuQHH69" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </ul>
+                <ul>
+                    <iframe width="700" height="380" src="https://www.youtube.com/embed/Z4P40hzv7OI?si=fNhwzZENgwuQHH69" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </ul>
             </center>
             <ul>
-            <p>Este video explora el síndrome del impostor, que se caracteriza por sentirse como un impostor en la vida. Se discuten las raíces del síndrome, patrones cognitivos y cómo afecta a las personas en su vida.</p>
+                <p>Este video explora el síndrome del impostor, que se caracteriza por sentirse como un impostor en la vida. Se discuten las raíces del síndrome, patrones cognitivos y cómo afecta a las personas en su vida.</p>
             </ul>
         </div>
         <div class="dividedList">
@@ -39,29 +41,29 @@
                 <p><br>En este segundo video, la persona comparte sus luchas con el síndrome del impostor y cómo ha afectado su vida. Habla de sus sentimientos de no pertenecer en varias situaciones, desde su trabajo hasta su experiencia de convertir a su perro en un perro de servicio. También destaca la importancia de encontrar comunidades en las que uno se sienta aceptado y representado.</p>
             </ul>
             <center>
-            <ul>
-            <iframe width="700" height="380" src="https://www.youtube.com/embed/g6wbCf0gNSw?si=dyjYtC7Trvq1uSh6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </ul>
+                <ul>
+                    <iframe width="700" height="380" src="https://www.youtube.com/embed/g6wbCf0gNSw?si=dyjYtC7Trvq1uSh6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </ul>
             </center>
         </div>
-    <hr>
-    <h1>Artículos</h1>
-    <br>
+        <hr>
+        <h1>Artículos</h1>
+        <br>
         <div class="dividedList2">
             <ul>
                 <p><br>Este artículo explora el síndrome del impostor y cómo afecta a las personas en su vida. Se discuten las causas del síndrome, cómo se manifiesta y cómo se puede superar.</p>
             </ul>
             <center>
-            <ul>
-                <button><a href="https://asana.com/es/resources/impostor-syndrome">Julia Martinez<br>asana</a></button>
-            </ul>
+                <ul>
+                    <button><a href="https://asana.com/es/resources/impostor-syndrome">Julia Martinez<br>asana</a></button>
+                </ul>
             </center>
         </div>
         <div class="dividedList2">
             <center>
-            <ul>
-                <button><a href="https://psicologianafria.com/sindrome-del-impostor/">Psicologìa Nafria</a></button>
-            </ul>
+                <ul>
+                    <button><a href="https://psicologianafria.com/sindrome-del-impostor/">Psicologìa Nafria</a></button>
+                </ul>
             </center>
             <ul>
                 <p><br>En este otro articulo habla sobre lo mismo que el anterior pero da diferentes maneras de poder afrontar este problema, asi mismo como el como prevenir que llegues a sufrir de este transtorno </p>
@@ -69,47 +71,51 @@
         </div>
         <div class="dividedList2">
             <ul>
-            <p><br>Este artículo, publicado en la revista "Harvard Business Review," destaca cómo el síndrome del impostor afecta a las mujeres en el lugar de trabajo, en particular a las mujeres de color, debido a los prejuicios sistémicos y el racismo. Se enfoca en cómo las culturas laborales tóxicas y los estereotipos de género y raza contribuyen a que las mujeres duden de sus logros.</p>
+                <p><br>Este artículo, publicado en la revista "Harvard Business Review," destaca cómo el síndrome del impostor afecta a las mujeres en el lugar de trabajo, en particular a las mujeres de color, debido a los prejuicios sistémicos y el racismo. Se enfoca en cómo las culturas laborales tóxicas y los estereotipos de género y raza contribuyen a que las mujeres duden de sus logros.</p>
             </ul>
             <center>
-            <ul>
-                <button><a href="https://hbr.org/2021/02/stop-telling-women-they-have-imposter-syndrome">Harvard Business Review</a></button>
-            </ul>
+                <ul>
+                    <button><a href="https://hbr.org/2021/02/stop-telling-women-they-have-imposter-syndrome">Harvard Business Review</a></button>
+                </ul>
         </div>
-    <hr>
-    <h1>Libros</h1>
-    <table>
-        <tr>
-            <th><strong>Libro</strong></th>
-            <th><strong>Descripción</strong></th>
-        </tr>
-        <tr>
-            <td>"The Secret Thoughts of Successful Women"</td>
-            <td>Explora el síndrome del impostor y ofrece consejos prácticos para superarlo.</td>
-        </tr>
-        <tr>
-            <td>"Lean Out: The Struggle for Gender Equality in Tech and Start-Up Culture"</td>
-            <td>Reúne ensayos sobre cuestiones de género, diversidad y el síndrome del impostor en la industria tecnológica.</td>
-        </tr>
-        <tr>
-            <td>"Presence: Bringing Your Boldest Self to Your Biggest Challenges"</td>
-            <td>Explora la autoestima y la confianza en situaciones laborales y personales.</td>
-        </tr>
-        <tr>
-            <td>"Own Your Greatness: Overcome Impostor Syndrome, Beat Self-Doubt, and Succeed in Life"</td>
-            <td>Proporciona estrategias para superar el síndrome del impostor y desarrollar la confianza en uno mismo.</td>
-        </tr>
-        <tr>
-            <td>"The Imposter Syndrome Remedy: A 30-Day Action Plan to Stop Feeling Like a Fraud"</td>
-            <td>Ofrece un plan de 30 días para abordar y superar el síndrome del impostor.</td>
-        </tr>
-        <tr>
-            <td>"The Confidence Code: The Science and Art of Self-Assurance—What Women Should Know"</td>
-            <td>Aborda la confianza y la autoestima en el contexto de las mujeres en el trabajo.</td>
-        </tr>
-    </table>
-    <br><br>
-    <hr>
+        <hr>
+        <h1>Libros</h1>
+        <table>
+            <tr>
+                <th>Libro</th>
+                <th>Descripción</th>
+            </tr>
+            <tr>
+                <td>"The Secret Thoughts of Successful Women"</td>
+                <td>Explora el síndrome del impostor y ofrece consejos prácticos para superarlo.</td>
+            </tr>
+            <tr>
+                <td>"Lean Out: The Struggle for Gender Equality in Tech and Start-Up Culture"</td>
+                <td>Reúne ensayos sobre cuestiones de género, diversidad y el síndrome del impostor en la industria tecnológica.</td>
+            </tr>
+            <tr>
+                <td>"Presence: Bringing Your Boldest Self to Your Biggest Challenges"</td>
+                <td>Explora la autoestima y la confianza en situaciones laborales y personales.</td>
+            </tr>
+            <tr>
+                <td>"Own Your Greatness: Overcome Impostor Syndrome, Beat Self-Doubt, and Succeed in Life"</td>
+                <td>Proporciona estrategias para superar el síndrome del impostor y desarrollar la confianza en uno mismo.</td>
+            </tr>
+            <tr>
+                <td>"The Imposter Syndrome Remedy: A 30-Day Action Plan to Stop Feeling Like a Fraud"</td>
+                <td>Ofrece un plan de 30 días para abordar y superar el síndrome del impostor.</td>
+            </tr>
+            <tr>
+                <td>"The Confidence Code: The Science and Art of Self-Assurance—What Women Should Know"</td>
+                <td>Aborda la confianza y la autoestima en el contexto de las mujeres en el trabajo.</td>
+            </tr>
+        </table>
+        <br><br>
+        <hr>
     </div>
 </body>
+<?php
+include_once '../include/footer.php';
+?>
+
 </html>
