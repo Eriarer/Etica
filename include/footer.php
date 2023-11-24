eb172fc4722403e58c654230eb346434eb8472a7
mariana-avila-rivera <marianaavilarivera@gmail.com>
2023-11-23 18:35:03 -0600

    footer con tabla y estilos

--- include/footer.php
+++ include/footer.php
@@ -1,28 +1,72 @@
-<link rel="stylesheet" href="<?php echo $_SERVER['ROOT'] = "http://" . $_SERVER['SERVER_NAME'] . "/Etica/" . 'css/footer.css' ?>">
+<link rel="stylesheet" href="../css/footer.css">
 
+<style>
+    .footer body {
+        margin: 0;
+        padding: 0;
+        font-family: Arial, sans-serif;
+        border: none;
+        width: 100%;
 
-<footer class="footerDiv">
-    <div>
-        <h4>Autores</h4>
-        <ul>
-            <li>Mariana Ávila Rivera</li>
-            <li>Gerardo Diosdado Escalera</li>
-            <li>Leonardo Daniel Gutiérrez Flores</li>
-            <li>Abraham Melgoza de la Torre</li>
-        </ul>
-    </div>
-    <div>
-        <h4>Bibliografía</h4>
-        <ul>
-            <li><a href="https://asana.com/es/resources/impostor-syndrome">https://asana.com/es/resources/impostor-syndrome</a></li>
-            <li><a href="https://www.gaceta.unam.mx/que-es-el-sindrome-del-impostor/">https://www.gaceta.unam.mx/que-es-el-sindrome-del-impostor/</a></li>
-            <li><a href="https://unamglobal.unam.mx/global_revista/sindrome-del-impostor-afecta-al-70-de-la-poblacion-de-que-se-trata/#:~:text=Siete%20de%20cada%2010%20personas,%2C%20capacidad%2C%20talento%20o%20creatividad.">https://unamglobal.unam.mx/global_revista/sindrome-del-impostor-afecta-al-70-de-la-poblacion-de-que-se-trata/</a></li>
-            <li><a href="https://www.unir.net/salud/revista/sindrome-del-impostor/">https://www.unir.net/salud/revista/sindrome-del-impostor/</a></li>
-            <li><a href="https://scholarworks.uvm.edu/tvc/vol34/iss1/6/">https://scholarworks.uvm.edu/tvc/vol34/iss1/6/</a></li>
-            <li><a href="https://so06.tci-thaijo.org/index.php/IJBS/article/view/521">https://so06.tci-thaijo.org/index.php/IJBS/article/view/521</a></li>
-            <li><a href="https://www.essentiallysports.com/f1-news-i-beat-myself-a-lot-over-it-mclarens-norris-opens-up-on-mental-struggles/">https://www.essentiallysports.com/f1-news-i-beat-myself-a-lot-over-it-mclarens-norris-opens-up-on-mental-struggles/</a></li>
-            <li><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7174434/">https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7174434/</a></li>
-            <li><a href="https://www.terapify.com/blog/sindrome-del-impostor-que-es-y-como-superarlo/">https://www.terapify.com/blog/sindrome-del-impostor-que-es-y-como-superarlo/</a></li>
-        </ul>
-    </div>
-</footer>
+    }
+
+    footer {
+        padding: 20px;
+        text-align: center;
+        background: #564db5;
+
+    }
+
+    .footer table {
+        width: 100%;
+        border-collapse: collapse;
+        margin-bottom: 15px;
+        border: 0px;
+    }
+
+    .footer th,
+    .footer td {
+        padding: 8px;
+        text-align: left;
+    }
+
+    .footer th {
+        font-size: 1.7em;
+    }
+
+    .footer img {
+        max-width: 70%;
+        height: auto;
+    }
+</style>
+
+<!-- Contenido de tu página -->
+<div class="footer">
+    <footer>
+        <table>
+            <tr>
+                <td rowspan="3">
+                    <img src="<?php echo $_SERVER['ROOT'] . 'media/images/index/uaaLogo.png' ?>" alt="Logo de la Universidad Autónoma de Aguascalientes">
+
+                </td>
+                <th>Proyecto Estudiantil</th>
+                <td rowspan="3">
+                    <p>AUTORES</p>
+                    <ul>
+                        <li>Mariana Ávila Rivera</li>
+                        <li>Gerardo Diosdado Escalera</li>
+                        <li>Leonardo Daniel Gutiérrez Flores</li>
+                        <li>Abraham Melgoza de la Torre</li>
+                    </ul>
+                </td>
+            </tr>
+            <tr>
+                <td>Esta página es parte de un proyecto estudiantil para dar a conocer el Síndrome del Impostor sin fines de lucro.</td>
+            </tr>
+            <tr>
+                <td>Materia: Ética</td>
+            </tr>
+        </table>
+    </footer>
+
+</div>
