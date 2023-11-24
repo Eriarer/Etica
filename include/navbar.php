96dedbbf689dac0af50e6629d2b220ff1ce17149
mariana-avila-rivera <marianaavilarivera@gmail.com>
2023-11-24 12:18:16 -0600

    menu toggle

--- include/navbar.php
+++ include/navbar.php
@@ -3,7 +3,7 @@
 <nav class="navbar navbar-expand-lg nav-body">
   <a class="navbar-brand NerdFont" href="<?php echo $_SERVER['ROOT']  ?>"><i class="nf nf-custom-home navbarIcon"></i></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <span class="navbar-toggler-icon"><i class="nf nf-md-menu"></i></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
